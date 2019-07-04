75363dcb3041edb6755c25722a13299d1968d63a
Matteo Mei <50500950+MatteoMei@users.noreply.github.com>
2019-07-04 16:57:53 +0200

    Update arduino-test.ino

--- firmware/arduino-test/arduino-test.ino
+++ firmware/arduino-test/arduino-test.ino
@@ -1,89 +1,48 @@
-#define LENGTH 6
+#define BUFFER_LENGTH 128
 
-char s[9];
-int count = 0;
-
-unsigned char registro, valore;
+char dato[BUFFER_LENGTH];
+volatile int pos = 0, receive_flag = 0;
 
 void setup(){
-  Serial.begin(9600);
+  USART_init(16000000 / 16 / 9600 - 1);
+  DDRB |= 0B00100000;
 }
 
 void loop(){
-  if(Serial.available() > 0){
-    s[count++] = Serial.read();
+  //decido di acquisire l'input completo e poi di stamparlo tutto i una volta
+  dato[pos] = USART_read();
+  if(dato[pos] == '\n'){
+    //stampa tutto
+    USART_Transmit(dato, pos + 1);
+    pos = 0;
   }
-  if(count == LENGTH){
-    count = 0;
-    s[LENGTH - 1] = '\0';
-    
-    //interpretazione del comando, quasi sicuramente non funziona
-    for(int i = 0; s[i] != '\0'; i++){
-      if(s[i] == ' ') s[i] = '\0';
-    }
-    
-    registro = stringHexToNumber(&s[0]);
-    valore = stringHexToNumber(&s[3]);
-    
-    if(registro < 0x3F) _SFR_IO8(registro) = valore;
-    else _SFR_MEM8(registro) = valore;
-  }
+  else pos++;
 }
 
-int stringCompare(char *s, char *t){
-  while(*s == *t){
-    if(*s == '\0') return 1;
+void USART_init(unsigned int ubrr){
+  UBRR0H = (unsigned char)(ubrr >> 8);
+  UBRR0L = (unsigned char)ubrr;
 
-    s++;
-    t++;
-  }
-  return 0;
+  UCSR0A = 0B00000000;
+  UCSR0B = 0B00011000;
+  UCSR0C = 0B00000110;
+
 }
 
-int stringIsHex(char *s){
-  int i;
-  
-  for(i = 0; s[i] != '\0'; i++){
-    if(s[i] != '0' && s[i] != '1' && s[i] != '2' && s[i] != '3' && s[i] != '4' && s[i] != '5' && s[i] != '6' && s[i] != '7' && s[i] != '8' && s[i] != '9' && s[i] != 'A' && s[i] != 'B' && s[i] != 'C' && s[i] != 'D' && s[i] != 'E' && s[i] != 'F') return 0;
-  }
-  if(i != 0) return 1;
-  else return 0;
+unsigned char USART_read(){
+  while(!(UCSR0A & (1 << 7)));
+  unsigned char byte_ = UDR0;
+
+  return byte_;
 }
 
-unsigned char stringHexToNumber(char *s){
-  int len, mask = 1, i, cifra;
-  unsigned char risultato = 0;
+void USART_write(unsigned char byte_){
+  UDR0 = byte_;
+}
 
-  if(stringIsHex(s) == 1){
-    //get the length of the string
-    for(len = 0; s[len] != '\0'; len++);
-
-    if(len != 0){
-      for(i = len - 1; i >= 0; i--){
-        switch(s[i]){
-          case '0': cifra = 0; break;
-          case '1': cifra = 1; break;
-          case '2': cifra = 2; break;
-          case '3': cifra = 3; break;
-          case '4': cifra = 4; break;
-          case '5': cifra = 5; break;
-          case '6': cifra = 6; break;
-          case '7': cifra = 7; break;
-          case '8': cifra = 8; break;
-          case '9': cifra = 9; break;
-          case 'A': cifra = 10; break;
-          case 'B': cifra = 11; break;
-          case 'C': cifra = 12; break;
-          case 'D': cifra = 13; break;
-          case 'E': cifra = 14; break;
-          case 'F': cifra = 15; break; 
-        }
-        risultato += (cifra * mask);
-        mask *= 16;
-      }
-      return risultato;
-    }
-    else return -1;
+void USART_Transmit(unsigned char *data, int length){
+  for(int i = 0; i < length; i++){
+    while (!(UCSR0A & (1<<5)));
+    UDR0 = data[i];
   }
-  else return -1;
 }
